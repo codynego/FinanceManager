8666c67338102f3f78e1e82514279957c14e6a59
abednego emonena <emonenaabednego@gmail.com>
2024-07-27 00:10:57 +0100

    user views and serializer created

--- user/models.py
+++ user/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    first_name = models.CharField(max_length=30, null=False)
+    last_name = models.CharField(max_length=30, null=False)
+    username = models.CharField(max_length=30, null=False)
+    email = models.EmailField(max_length=30, null=False)
+    phone_number = models.CharField(max_length=30, null=False)
+    password = models.CharField(max_length=30, null=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.username
+    
+
+
+
+
