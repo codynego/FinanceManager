424af6b8c57faa2f09b9889ebd7700f094369c9e
abednego emonena <emonenaabednego@gmail.com>
2024-07-29 21:27:15 +0100

    view to get the authenticated user account info

--- account/urls.py
+++ account/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path, include
 # from .views import AccountListView, AccountDetailView
-from .views import AccountView
+from .views import AccountView, UserAccountView, UserAccount
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register(r'accounts', AccountView, basename="accounts")
+
+router2 = DefaultRouter()
+router2.register(r'account/me', AccountView, basename="user_accounts")
 
 
 
@@ -12,5 +15,6 @@ router.register(r'accounts', AccountView, basename="accounts")
 urlpatterns = [
     # path('Account/', AccountListView.as_view(), name='account-list'),
     # path('Account/<int:pk>/', AccountDetailView.as_view(), name='account-detail'),
-    path("", include(router.urls))
+    path("", include(router.urls)),
+    path('account/me/', UserAccount.as_view(), name='user_account')
 ]
--- account/views.py
+++ account/views.py
@@ -6,6 +6,8 @@ from .models import SavingsAccount
 from .serializers import AccountSerializer
 from rest_framework.permissions import IsAuthenticated
 from .permissions import GetAccountPermission
+from rest_framework.decorators import action
+from rest_framework.views import APIView
 
 
 # Create your views here.
@@ -15,11 +17,22 @@ class AccountView(viewsets.ModelViewSet):
     serializer_class = AccountSerializer
     permission_classes = [GetAccountPermission]
 
-class UserAccountView()(viewsets.ModelViewSet):
+class UserAccount(APIView):
+    def get(self, request):
+        try:
+            account = SavingsAccount.objects.get(user=request.user)
+            serializer = AccountSerializer(account)
+            return Response({"data": serializer.data, "status": status.HTTP_200_OK, "message": "account retrieved"})
+        except:
+            return Response({"data": None, "status": status.HTTP_404_NOT_FOUND, "message": "account not found"})
+
+class UserAccountView(viewsets.ModelViewSet):
     queryset = SavingsAccount.objects.all()
     serializer_class = AccountSerializer
     permission_classes = [IsAuthenticated]
 
+
+    @action(detail=False, methods=['get'])
     def list(self, request):
         try:
             data = SavingsAccount.objects.get(user=request.user)
